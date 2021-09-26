643494783127052cef572779ed0c37c19d367159
nihal9605 <90958929+nihal9605@users.noreply.github.com>
2021-09-26 23:28:42 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,12 +4,12 @@
 
 class Translation(object):
     
-    START_TEXT = """<b>Hey {}!!</b>
+    START_TEXT = """<b>Hey {} How Are You!!</b>
 <i>Am Just A Auto Filter Bot v10....😉
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+You Can't Add Me In Your Channel 🔥🔥😝
 
-For More Details Click Help Button Below..
+Join Our Movie's Group Click Below 😍..
 @Dark_Moviezz
 </i>"""    
     
@@ -48,11 +48,11 @@ For More Details Click Help Button Below..
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/nihal4_0">Nihal</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/nihal9605/Auto-filterbot-v10">Click Me</a></i>
 """
